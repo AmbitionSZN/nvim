3bc91f138870b064df1e044e184ca3db2efc1b7f
AmbitionSZN <jahlwalk22635@gmail.com>
2024-06-06 04:37:19 -0400

    got dapui working

--- lua/ambition/plugins/mason-nvim-dap.lua
+++ lua/ambition/plugins/mason-nvim-dap.lua
@@ -1,9 +1,9 @@
 return {
-
 	"jay-babu/mason-nvim-dap.nvim",
+	event = "VeryLazy",
 	dependencies = { "williamboman/mason.nvim",
 		"mfussenegger/nvim-dap" },
 	opts = {
 		handlers = {}
-	}
+	},
 }
